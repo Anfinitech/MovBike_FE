700dbc11031d764abe5bcda9b693303301ee4f12
Andrés Medina <anfmedinara@unal.edu.co>
2021-10-25 20:14:30 -0500

    Login style fixed scroll

--- src/views/Bikes.vue
+++ src/views/Bikes.vue
@@ -14,6 +14,7 @@ import CreateBike from "@/components/Bikes/CreateBike.vue";
 export default {
   name: "Bikes",
   components: { BikesTable, CreateBike },
+  
   data: function() {
     return{
       activeOption: 'BikesTable'
