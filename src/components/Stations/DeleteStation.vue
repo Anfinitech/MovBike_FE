2373f0cf5620e8874e90d21903e4dab15cbd57e5
Andrés Medina <anfmedinara@unal.edu.co>
2021-10-28 00:57:08 -0500

    All Station CRUD buttons linked with independent components

---
+++ src/components/Stations/DeleteStation.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>Eliminar una estación</h1>
+<button>Eliminar</button>
+  <button v-on:click.self.prevent="renderStationsTable">Volver</button>
+</template>
+
+
+<script>
+export default {
+  name: "DeleteStation",
+
+  methods: {
+    renderStationsTable: function () {
+      this.$emit("loadcomponent", "StationsTable");
+    },
+  },
+};
+</script>
