7a85668a929e8dede0c9ce9142e39ced1c14f2a0
Andrés Medina <anfmedinara@unal.edu.co>
2021-10-28 13:25:20 -0500

    HTML small changes in Delete

--- src/components/Bikes/DeleteBike.vue
+++ src/components/Bikes/DeleteBike.vue
@@ -1,5 +1,12 @@
 <template>
   <h1>Eliminar una bicicleta</h1>
+  <br />
+  <div>
+    <p>ID: </p>
+    <p>Condición: </p>
+    <p>Ubicación: </p>
+  </div>
+  <br />
   <button>Eliminar</button>
   <button v-on:click.self.prevent="renderStationsTable">Volver</button>
 </template>
--- src/components/Stations/DeleteStation.vue
+++ src/components/Stations/DeleteStation.vue
@@ -1,5 +1,17 @@
 <template>
   <h1>Eliminar una estación</h1>
+  <br />
+  <div>
+    <p>ID:</p>
+    <p>Nombre:</p>
+    <p>Estado:</p>
+    <p>Capacidad:</p>
+    <p>Ocupación:</p>
+    <p>Bicis D:</p>
+    <p>Bicis ND:</p>
+    <p>Bicis T:</p>
+  </div>
+  <br />
   <button>Eliminar</button>
   <button v-on:click.self.prevent="renderStationsTable">Volver</button>
 </template>
