f7c87055c710fe5e5cfae92c4ea8fe7336ab269c
Andrés Medina <anfmedinara@unal.edu.co>
2021-10-28 01:41:57 -0500

    User view file and component file created

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -1,13 +1,27 @@
 <template>
   <div>
-    <h1>Usuarios [Beta]</h1>
-    <div></div>
+    <h1>Usuarios</h1>
+    <div>
+      <component @loadcomponent="renderUserComponent" :is="activeOption" />
+    </div>
   </div>
 </template>
 
 <script>
+import UsersTable from "@/components/Stations/StationsTable.vue";
+
 export default {
-  name: "Users",
-  components: {},
+  name: "Stations",
+  components: { UsersTable },
+  data: function () {
+    return {
+      activeOption: "UsersTable",
+    };
+  },
+  methods: {
+    renderUserComponent(message){
+      this.activeOption = message;
+    }
+  },
 };
 </script>
