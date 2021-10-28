d2714f4a22f6729578bae7ee9b2bd8ec6ab3aa35
1098744871 <ivonne.suarez93@gmail.com>
2021-10-28 11:43:41 -0500

    UpdateBike sin funcionalidad

--- src/components/Bikes/UpdateBike.vue
+++ src/components/Bikes/UpdateBike.vue
@@ -1,13 +1,81 @@
 <template>
-  <h1>Actualizar bicicleta</h1>
-  <button>Actualizar</button>
-  <button v-on:click.self.prevent="renderStationsTable">Volver</button>
+  <div class="general-container">
+    <h1>Actualizar bicicleta</h1>
+    <form
+      name="form"
+      id="form"
+      method="post"
+      enctype="multipart/form-data"
+      v-on:submit.prevent="actualizar()"
+    >
+      <h3 class="title">Bicicleta</h3>
+      <br />
+      <div class="form-group">
+        <p>Condición:</p>
+        <input
+          type="radio"
+          name="condicion"
+          id="buena"
+          value="true"
+          v-model="condicion"
+        />
+        <label class="rad" for="buena">En buen estado</label>
+        <br />
+        <input
+          type="radio"
+          name="condicion"
+          id="averiada"
+          value="false"
+          v-model="condicion"
+        />
+        <label class="rad" for="averiada">Averiada</label>
+      </div>
+      <div class="form-group">
+        <p>Ubicación:</p>
+
+        <select v-model="bikeCreation.creation_data.current_station">
+          <option disabled selected>Seleccione una estacion</option>
+          <option
+            v-for="station in stations"
+            :key="station.e_id"
+            :value="station.e_id"
+          >
+            {{ station.e_nombre }}
+          </option>
+        </select>
+      </div>
+      <br />
+      <button class="boton" v-on:click.self.prevent="renderStationsTable">Volver</button>
+      <button class="boton">Actualizar</button>
+    </form>
+
+    <p class="caja">
+      Haciendo seguimiento continuo a cada nodo para mejorar nuestro servicio y
+      la experiencia de usuario.
+    </p>
+    <v-container fluid>
+      <img src="@/assets/stations/actualizar.jpg" alt="" />
+    </v-container>
+  </div>
 </template>
 
 
 <script>
+import axios      from 'axios';
 export default {
   name: "UpdateBike",
+  data: function() {
+    return {
+      bikeCreation: {
+        b_id : 0,
+        creation_data: {
+          condicion: 0,
+          current_station : 0,
+        }
+      },
+      stations: [],
+    }
+  },
 
   methods: {
     renderStationsTable: function () {
@@ -16,3 +84,41 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+/*------------Formulario------------*/
+form {
+  width: 370px;
+  padding: 30px;
+  font-size: 18px;
+  float: right;
+  margin-right: 10%;
+  border: 2px double purple;
+  margin-top: 3%;
+}
+
+.form-control {
+  width: 300px;
+}
+.rad {
+  font-size: 15px;
+}
+.boton {
+  width: 80px;
+  font-size: 12px;
+  height: 22px;
+}
+/*------------Mensaje--------------*/
+.caja {
+  font-family: sans-serif;
+  font-size: 20px;
+  width: 500px;
+  margin-left: 10%;
+  margin-top: 30px;
+  overflow: hidden;
+}
+/*--------------Imagen-------------*/
+v-container {
+  margin-left: 10%;
+}
+</style>
