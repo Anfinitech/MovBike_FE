1956bd4ce60808e553d8b9ff2ab655076a0aa3c0
Andrés Medina <anfmedinara@unal.edu.co>
2021-10-29 17:37:29 -0500

    Stations Full CRUD Auth

--- src/components/Stations/DeleteStation.vue
+++ src/components/Stations/DeleteStation.vue
@@ -1,30 +1,129 @@
 <template>
-  <h1>Eliminar una estación</h1>
-  <br />
-  <div>
-    <p>ID:</p>
-    <p>Nombre:</p>
-    <p>Estado:</p>
-    <p>Capacidad:</p>
-    <p>Ocupación:</p>
-    <p>Bicis D:</p>
-    <p>Bicis ND:</p>
-    <p>Bicis T:</p>
+  <div v-if="loaded">
+    <h1>Eliminar una estación</h1>
+    <br />
+    <div>
+      <p>ID: {{ estacion.e_id }}</p>
+      <p>Nombre: {{ estacion.e_nombre }}</p>
+      <p>Estado: {{ estacion.e_estado }}</p>
+      <p>Capacidad: {{ estacion.e_capacidad }}</p>
+      <p>Ocupación:{{ estacion.e_ocupacion }}</p>
+      <p>Bicis D: {{ estacion.e_bicicletasD }}</p>
+      <p>Bicis ND: {{ estacion.e_bicicletasND }}</p>
+      <p>Bicis T: {{ estacion.e_bicicletasT }}</p>
+    </div>
+    <br />
+    <button v-on:click="deleteStation">Eliminar</button>
+    <button v-on:click.self.prevent="renderStationsTable">Volver</button>
   </div>
-  <br />
-  <button>Eliminar</button>
-  <button v-on:click.self.prevent="renderStationsTable">Volver</button>
 </template>
 
 
 <script>
+import axios from "axios";
+
 export default {
   name: "DeleteStation",
+
+  data() {
+    return {
+      estacion: {},
+      loaded: false,
+    };
+  },
 
   methods: {
     renderStationsTable: function () {
       this.$emit("loadcomponent", "StationsTable");
     },
+
+    getData: async function () {
+      await this.verifyToken();
+
+      if (
+        localStorage.getItem("tokenRefresh") === null ||
+        localStorage.getItem("tokenAccess") === null
+      ) {
+        return;
+      }
+
+      let id = localStorage.getItem("StationIdToDelete");
+      let url = "https://move-and-flow-be.herokuapp.com";
+      axios
+        .get(url + "/estaciones/" + id + "/", {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("tokenAccess")}`,
+          },
+        })
+        .then((response) => {
+          console.log(response.data);
+          this.estacion = response.data;
+          this.loaded = true;
+        })
+        .catch((error) => {
+          console.log(error.response);
+        });
+    },
+
+    deleteStation: async function() {
+      await this.verifyToken();
+
+      if (
+        localStorage.getItem("tokenRefresh") === null ||
+        localStorage.getItem("tokenAccess") === null
+      ) {
+        return;
+      }
+
+      let url = "https://move-and-flow-be.herokuapp.com";
+      axios
+        .delete(url + "/estaciones/" + this.estacion.e_id + "/", {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("tokenAccess")}`,
+          },
+        })
+        .then((response) => {
+          alert(response.data);
+          console.log(response);
+          this.renderStationsTable();
+        })
+        .catch((error) => {
+          console.log(error.response);
+        });
+    },
+
+    verifyToken: async function () {
+      if (
+        localStorage.getItem("tokenRefresh") === null ||
+        localStorage.getItem("tokenAccess") === null
+      ) {
+        this.accessDenied();
+        return;
+      }
+
+      return axios
+        .post(
+          "https://move-and-flow-be.herokuapp.com/refresh/",
+          { refresh: localStorage.getItem("tokenRefresh") },
+          { headers: {} }
+        )
+        .then((result) => {
+          localStorage.setItem("tokenAccess", result.data.access);
+        })
+        .catch((error) => {
+          this.accessDenied();
+        });
+    },
+
+    accessDenied: function () {
+      localStorage.clear();
+      alert("Acceso Denegado. Vuelve a iniciar sesión.");
+      this.$router.push({ name: "Login" });
+    },
+  },
+
+  created: async function() {
+    this.getData();
   },
 };
 </script>
--- src/components/Stations/UpdateStation.vue
+++ src/components/Stations/UpdateStation.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="general-container">
+  <div class="general-container" v-if="loaded">
     <h1>Actualizar estación</h1>
     <form
       name="form"
@@ -8,13 +8,13 @@
       enctype="multipart/form-data"
       v-on:submit.prevent="actualizar()"
     >
-      <h3 class="title">Estación {{ station.nombre }}</h3>
+      <h3 class="title">Estación {{ station.e_nombre }}</h3>
       <br />
       <div class="form-group">
         <label for="nombre" class="txt_negrita">Nombre:</label>
         <br />
         <input
-          v-model="station.nombre"
+          v-model="station.e_nombre"
           type="text"
           id="name"
           name="name"
@@ -27,18 +27,16 @@
         <input
           type="radio"
           name="estado"
-          id="abierta"
-          value="true"
-          v-model="estado"
+          value="Abierta"
+          v-model="station.e_estado"
         />
         <label class="rad" for="abierta">Abierta</label>
         <br />
         <input
           type="radio"
           name="estado"
-          id="cerrada"
-          value="false"
-          v-model="estado"
+          value="Cerrada"
+          v-model="station.e_estado"
         />
         <label class="rad" for="cerrada">Cerrada</label>
       </div>
@@ -46,15 +44,18 @@
         <label for="capacidad" class="txt_negrita">Capacidad:</label>
         <br />
         <input
-          type="int"
+          type="number"
+          min="1"
           id="capacidad"
           name="capacidad"
           class="form-control"
-          v-model="station.capacidad"
+          v-model="station.e_capacidad"
         />
       </div>
       <br />
-      <button class="boton">Actualizar</button>
+      <button class="boton" v-on:click.self.prevent="updateStation">
+        Actualizar
+      </button>
       <button class="boton" v-on:click.self.prevent="renderStationsTable">
         Volver
       </button>
@@ -75,12 +76,8 @@ export default {
   name: "UpdateStation",
   data: function () {
     return {
-      station: {
-        e_id: "id",
-        e_nombre: "nombre",
-        e_estado: "estado",
-        e_capacidad: "capacidad",
-      },
+      station: {},
+      loaded: false,
     };
   },
 
@@ -88,6 +85,103 @@ export default {
     renderStationsTable: function () {
       this.$emit("loadcomponent", "StationsTable");
     },
+
+    getData: async function () {
+      await this.verifyToken();
+
+      if (
+        localStorage.getItem("tokenRefresh") === null ||
+        localStorage.getItem("tokenAccess") === null
+      ) {
+        return;
+      }
+
+      let id = localStorage.getItem("StationIdToUpdate");
+      let url = "https://move-and-flow-be.herokuapp.com";
+
+      axios
+        .get(url + "/estaciones/" + id + "/", {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("tokenAccess")}`,
+          },
+        })
+        .then((response) => {
+          console.log(response.data);
+          this.station = response.data;
+          this.loaded = true;
+        })
+        .catch((error) => {
+          console.log(error.response);
+        });
+    },
+
+    updateStation: async function () {
+      await this.verifyToken();
+
+      if (
+        localStorage.getItem("tokenRefresh") === null ||
+        localStorage.getItem("tokenAccess") === null
+      ) {
+        return;
+      }
+      let url = "https://move-and-flow-be.herokuapp.com";
+      let estacionUpdate = {
+        e_nombre: this.station.e_nombre,
+        e_estado: this.station.e_estado === "Abierta",
+        e_capacidad: this.station.e_capacidad,
+      };
+
+      console.log(estacionUpdate);
+
+      axios
+        .patch(url + "/estaciones/" + this.station.e_id + "/", estacionUpdate, {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("tokenAccess")}`,
+          },
+        })
+        .then((result) => {
+          alert("Actualización exitosa.");
+          console.log(estacionUpdate);
+          console.log(result.data);
+          this.renderStationsTable();
+        })
+        .catch((error) => {
+          console.log(estacionUpdate);
+          console.log(error.response);
+        });
+    },
+
+    verifyToken: async function () {
+      if (
+        localStorage.getItem("tokenRefresh") === null ||
+        localStorage.getItem("tokenAccess") === null
+      ) {
+        this.accessDenied();
+        return;
+      }
+
+      return axios
+        .post(
+          "https://move-and-flow-be.herokuapp.com/refresh/",
+          { refresh: localStorage.getItem("tokenRefresh") },
+          { headers: {} }
+        )
+        .then((result) => {
+          localStorage.setItem("tokenAccess", result.data.access);
+        })
+        .catch((error) => {
+          this.accessDenied();
+        });
+    },
+    accessDenied: function () {
+      localStorage.clear();
+      alert("Acceso Denegado. Vuelve a iniciar sesión.");
+      this.$router.push({ name: "Login" });
+    },
+  },
+
+  created: async function () {
+    this.getData();
   },
 };
 </script>
