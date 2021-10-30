3a09e8526e5dcb3acf40e7728355ad49704840e4
Gustavo Briceño <gustavo7490@gmail.com>
2021-10-30 14:32:53 -0500

    Delete css funcional

--- src/components/Users/DeleteUser.vue
+++ src/components/Users/DeleteUser.vue
@@ -1,17 +1,23 @@
 <template>
-  <h1>Eliminar usuario</h1>
-  <br />
-  <div>
-          <p>ID: </p>
-          <p>Alias: </p>
-          <p>Nombre: </p>
-          <p>Correo Electrónico: </p>
-          <p>Rol: </p>
-          <p>Acciones: </p>
+<div class="general-container">
+  <div class="p-container">
+  <div class="general-title"><h1>Eliminar usuario</h1></div>
+  <div class="prevention"><p>¿Esta seguro de eliminar la siguiente información?</p></div>
+  <div class="group">
+          <p class="sub-title">ID: </p>
+          <p class="sub-title">Alias: </p>
+          <p class="sub-title">Nombre: </p>
+          <p class="sub-title">Correo Electrónico: </p>
+          <p class="sub-title">Rol: </p>
+          <p class="sub-title">Acciones: </p>
   </div>
   <br />
-  <button>Eliminar</button>
-  <button v-on:click.self.prevent="renderUsersTable">Volver</button>
+  <div class="botones">
+  <button class="boton_delete" ><fa icon="trash" class="icon"/>Eliminar</button>
+  <button class="boton_back" v-on:click.self.prevent="renderUsersTable"><fa icon="undo" class="icon"/>Volver</button>
+  </div>
+  </div>
+</div>
 </template>
 
 
@@ -26,3 +32,106 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+
+.general-container {
+  height: 100%;
+  width: 100%;
+  border-radius: 20px;
+  display: flex;
+  justify-content: center;
+  overflow: hidden;
+  background-image: url("../../assets/stations/DeleteStations.jpg");
+  background-size: cover;
+  background-repeat: no-repeat;
+  padding: 1rem;
+}
+
+
+.general-title {
+  display: flex;
+  justify-content: space-around;
+  position: initial;
+  padding-bottom: 30px;
+  margin-top: 20px;
+}
+
+.p-container {
+  color: #5046af;
+  flex-direction: column;
+  align-items: center;
+  text-align: left;
+  padding: 1rem 1.5rem;
+  width: 410px;
+  background-color: rgba(233, 233, 233, 0.918);
+  border-radius: 20px;
+  backdrop-filter: blur(10px);
+  margin: 20px;
+  box-shadow: 0 0 10px rgb(190, 190, 190);
+}
+
+.prevention{
+  text-align: center;
+  margin-bottom: 15px;
+}
+
+.prevention p{
+  color: crimson;
+  font-weight: 600;
+}
+
+.group .sub-title{
+  color: #5046af;
+  font-weight: 600;
+}
+
+
+.botones {
+  display: flex;
+  flex-direction: row;
+  justify-content:center;
+  margin-top: 15px;
+}
+
+.boton_delete {
+  padding: 10px 10px;
+  position: relative;
+  margin: 3px 5px 5px;
+  text-decoration: none;
+  background-color: crimson;
+  border: none;
+  border-radius: 7px;
+  font-weight: 600;
+  color: var(--white);
+  cursor: pointer;
+  display: inline;
+}
+
+.boton_back {
+  padding: 10px 10px;
+  position: relative;
+  margin: 3px 0px 5px;
+  text-decoration: none;
+  background-color: #00c2a8;
+  border: none;
+  border-radius: 7px;
+  font-weight: 600;
+  color: var(--white);
+  cursor: pointer;
+}
+
+.boton_back:hover {
+  background-color: var(--white);
+  color: #00c2a8;
+}
+
+.boton_delete:hover {
+  background-color: var(--white);
+  color: crimson;
+}
+
+.icon{
+  margin-right: 5px;
+}
+</style>
