a5e86cd2e4e7776ef162b677edfa8258032b098c
Andrés Medina <anfmedinara@unal.edu.co>
2021-10-30 06:57:16 -0500

    Axios First Test

--- src/components/Users/CreateUser.vue
+++ src/components/Users/CreateUser.vue
@@ -45,7 +45,7 @@
         />
       </p>
       <div class="form-group">
-        <p>Rol: </p>
+        <p>Rol:</p>
         <input
           type="text"
           name="u_rol"
@@ -60,7 +60,7 @@
         Volver
       </button>
     </form>
-    
+
     <p class="caja">
       Construyendo un nodo de bienestar para nuestra comunidad.
     </p>
@@ -90,8 +90,66 @@ export default {
     renderUsersTable: function () {
       this.$emit("loadcomponent", "UsersTable");
     },
-    createUser: function () {
+    createUser: async function () {
+      await this.verifyToken();
 
+      if (
+        localStorage.getItem("tokenRefresh") === null ||
+        localStorage.getItem("tokenAccess") === null
+      ) {
+        return;
+      }
+
+      let url = "https://move-and-flow-be.herokuapp.com";
+      axios
+        .post(url + "/users/", this.nuevoUsuario, {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("tokenAccess")}`,
+          },
+        })
+        .then((response) => {
+          alert(response.data);
+          console.log(response.data);
+        })
+        .catch((error) => {
+          console.log(error.response);
+
+          if (error.response.status == "401") {
+            this.accessDenied();
+          } else if (error.response.status == "400") {
+            alert("BAD REQUEST [400]");
+          } else {
+            alert(error);
+          }
+        });
+    },
+
+    verifyToken: async function () {
+      if (
+        localStorage.getItem("tokenRefresh") === null ||
+        localStorage.getItem("tokenAccess") === null
+      ) {
+        this.accessDenied();
+        return;
+      }
+
+      return axios
+        .post(
+          "https://move-and-flow-be.herokuapp.com/refresh/",
+          { refresh: localStorage.getItem("tokenRefresh") },
+          { headers: {} }
+        )
+        .then((result) => {
+          localStorage.setItem("tokenAccess", result.data.access);
+        })
+        .catch((error) => {
+          this.accessDenied();
+        });
+    },
+    accessDenied: function () {
+      localStorage.clear();
+      alert("Acceso Denegado. Vuelve a iniciar sesión.");
+      this.$router.push({ name: "Login" });
     },
   },
 };
