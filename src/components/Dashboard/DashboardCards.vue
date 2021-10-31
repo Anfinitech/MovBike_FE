42eabced64e2d5cb99923e670795f20efc9d83b1
Andrés Medina <anfmedinara@unal.edu.co>
2021-10-30 19:55:35 -0500

    Click in dashboardcards redirects to intended page

--- src/components/Dashboard/DashboardCards.vue
+++ src/components/Dashboard/DashboardCards.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="cards">
-    <div class="card-single">
-      <div>
-        <h2>Estaciones</h2>
+    <div class="card-single" @click.self.prevent="loadStations()">
+      <div @click.self.prevent="loadStations()">
+        <h2 @click.self.prevent="loadStations()">Estaciones</h2>
       </div>
-      <div>
-        <img src="@/assets/home/img/Estacion.svg" width="250px" alt="" />
+      <div @click.self.prevent="loadStations()">
+        <img src="@/assets/home/img/Estacion.svg" width="250px" alt="" @click.self.prevent="loadStations()"/>
       </div>
     </div>
-    <div class="card-single">
-      <div>
-        <h2>Bicicletas</h2>
+    <div class="card-single" @click.self.prevent="loadBikes()">
+      <div @click.self.prevent="loadBikes()">
+        <h2 @click.self.prevent="loadBikes()">Bicicletas</h2>
       </div>
-      <div>
-        <img src="@/assets/home/img/Bicis.svg" width="200px" alt="" />
+      <div @click.self.prevent="loadBikes()">
+        <img src="@/assets/home/img/Bicis.svg" width="200px" alt="" @click.self.prevent="loadBikes()"/>
       </div>
     </div>
   </div>
@@ -25,5 +25,13 @@
 export default {
   name: "DashboardCards",
   components: {},
+  methods:{
+    loadBikes: function () {
+      this.$router.push({ name: "Bikes" });
+    },
+    loadStations: function () {
+      this.$router.push({ name: "Stations" });
+    },
+  }
 };
 </script>
